d2f3591e17fa203202feb037f0e38799f0f7d86a
Géraud Vercasson <geraud.vercasson@iotize.com>
2018-10-15 15:53:22 +0200

    Initial commit

--- src/app/iotize/device/device.service.ts
+++ src/app/iotize/device/device.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { IoTizeDevice } from '@iotize/device-client.js/device';
+import { ComProtocol } from '@iotize/device-client.js/protocol/api';
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +8,32 @@ import { Injectable } from '@angular/core';
 export class DeviceService {
 
   isReady = false;
-  // device: IoTizeDevice;
+  device: IoTizeDevice;
   connectionPromise: Promise<void>;
 
   constructor() { }
+
+
+  async init(protocol: ComProtocol) {
+    this.isReady = false;
+    try {
+      this.device = IoTizeDevice.create();
+      console.log('device created');
+      this.connectionPromise = this.connect(protocol);
+      await this.connectionPromise;
+      this.isReady = true;
+    } catch (error) {
+      console.error('init failed');
+      console.error(error);
+      throw new Error('Connection Failed');
+    }
+  }
+
+  connect(protocol: ComProtocol): Promise<void> {
+    return this.device.connect(protocol);
+  }
+
+  disconnect(): Promise<void> {
+    return this.device.disconnect();
+  }
 }
