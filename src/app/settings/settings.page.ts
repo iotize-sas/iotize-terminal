5ffb517c3b33b30213392cd6faf8c60fda6a90cf
Géraud Vercasson <geraud.vercasson@iotize.com>
2018-10-17 16:38:44 +0200

    settings has its own service

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
-import { TerminalService } from './../iotize/terminal.service';
-import { Component } from '@angular/core';
+import { SettingsService } from './settings.service';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -8,5 +9,71 @@ import { Component } from '@angular/core';
 })
 export class SettingsPage {
 
-  constructor(public terminal: TerminalService) {}
+  loader: HTMLIonLoadingElement;
+
+  constructor(public settings: SettingsService,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    public changeDetector: ChangeDetectorRef) { }
+
+  async changeSettings() {
+    console.log('change settings');
+    if (this.settings.settingsHasChanged()) {
+      const confirm = await this.alertCtrl.create({
+        header: 'Apply new settings',
+        message: 'Are you sure?',
+        buttons: [
+          {
+            text: 'No',
+            role: 'cancel',
+            handler: () => {
+            }
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              this.applyChanges();
+            }
+          },
+        ]
+      });
+      await confirm.present();
+    }
+  }
+
+  async applyChanges() {
+    await this.loadingMessage('Applying new settings');
+    try {
+      await this.settings.applyChanges();
+      this.loader.dismiss();
+    } catch (error) {
+      this.loader.dismiss();
+      console.log(error);
+      throw error;
+    }
+  }
+  async readSettingsFromTap() {
+    await this.loadingMessage('Reading settings from tap');
+    try {
+      await this.settings.getUARTSettings();
+      this.loader.dismiss();
+    } catch (error) {
+      this.loader.dismiss();
+      console.error(error);
+    }
+  }
+
+  async loadingMessage(message: string) {
+    if (this.loader === undefined) {
+      this.loader = await this.loadingCtrl.create();
+    }
+    this.loader.message = message;
+    this.loader.present();
+  }
 }
+  // Doesn't work with tabs?
+  // ionViewCanLeave(): Promise<boolean> {
+  //   console.log('ViewCanLeave?');
+  // return this.changeSettings();
+  // }
+
---
+++ src/app/settings/settings.service.ts
@@ -0,0 +1,114 @@
+import { DeviceService } from './../iotize/device/device.service';
+import { LoggerService } from './../iotize/logger.service';
+import { Injectable } from '@angular/core';
+import { UartSettings } from '@iotize/device-client.js/device/model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+
+  public BAUD_RATES: Array<number> = [110, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200,
+    187500, 230400, 460800, 921600, 1843200, 3686400];
+  dataType: 'ASCII' | 'HEX' = 'ASCII';
+  endOfLine: Array<string> = [];
+
+  _settings: UartSettings; // real settings
+  settings: UartSettings; // displayed settings
+
+  constructor(public logger: LoggerService,
+    public deviceService: DeviceService) {
+    this._settings = {
+      physicalPort: 'USB',
+      stopBit: 'ONE',
+      parity: 'NONE',
+      dataBitsLength: 8,
+      handshake: 'NONE',
+      handshakeDelimiter: 'NONE',
+      timeout: 50,
+      baudRate: 187500,
+      ofs: 0
+    };
+    this.settings = Object.assign({}, this._settings);
+  }
+
+  async getUARTSettings(): Promise<void> {
+    try {
+      const response = await this.deviceService.device.service.target.getUARTSettings();
+
+      if (response.isSuccess()) {
+        this._settings = response.body();
+        this.settings = Object.assign({}, this._settings);
+        return;
+      }
+      throw new Error('getUARTSettings response failed');
+
+    } catch (error) {
+      this.logger.log('error', error);
+    }
+  }
+
+  async setUARTSettings(): Promise<void> {
+    try {
+      await this.deviceService.device.service.target.postDisconnect();
+
+      const response = await this.deviceService.device.service.target.setUARTSettings(this._settings);
+
+      if (response.isSuccess()) {
+        await this.deviceService.device.service.target.postConnect();
+        return;
+      }
+      throw new Error('setUARTSettings response failed');
+
+    } catch (error) {
+      this.logger.log('error', error);
+      throw (error);
+    }
+  }
+
+  // These are to be able to bind ofs to a select button
+  // otherwise a 0 value automatically sets itself as undefined
+
+  get ofs() {
+    return this.settings.ofs.toString();
+  }
+
+  set ofs(value: string) {
+    this.settings.ofs = Number(value);
+  }
+
+  async applyChanges() {
+    // REAL IMPLEMENTATION
+    // try {
+    //   this._settings = Object.assign({}, this.settings);
+    //   await this.setUARTSettings();
+    // } catch (error) {
+    //   this.logger.log('error', error);
+    // }
+
+    // MOCKED IMPLEMENTATION
+    console.log('mocked TerminalService.applyChanges');
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        this._settings = Object.assign({}, this.settings);
+        this.logger.log('info', 'resolved');
+        console.log('resolved');
+        resolve();
+      }, 2000);
+    });
+  }
+
+  discardChanges() {
+    this.settings = Object.assign({}, this._settings);
+  }
+
+  settingsHasChanged(): boolean {
+
+    for (const prop in this._settings) {
+      if (this.settings[prop] !== this._settings[prop]) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
