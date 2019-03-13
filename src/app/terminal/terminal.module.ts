dd6e7ccc0d2b711f43ecccea0ad8f033da98167f
GVercasson <geraud.vercasson@iotize.com>
2019-03-13 15:49:58 +0100

    fix: ionic4 update modal

--- src/app/terminal/terminal.module.ts
+++ src/app/terminal/terminal.module.ts
@@ -1,5 +1,4 @@
 import { TerminalModalPageModule } from './terminal-modal/terminal-modal.module';
-import { TerminalModalPage } from './terminal-modal/terminal-modal.page';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
@@ -15,7 +14,6 @@ import { TerminalPage } from './terminal.page';
     TerminalModalPageModule,
     RouterModule.forChild([{ path: '', component: TerminalPage }])
   ],
-  declarations: [TerminalPage],
-  entryComponents: [TerminalModalPage]
+  declarations: [TerminalPage]
 })
 export class TerminalPageModule {}
