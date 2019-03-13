92ce2565f7a14cff0976e5350e018b6c67761323
GVercasson <geraud.vercasson@iotize.com>
2019-03-13 15:44:33 +0100

    clean up

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { TabsPage } from './tabs.page';
-import { HomePage } from '../home/home.page';
-import { TerminalPage } from '../terminal/terminal.page';
-import { SettingsPage } from '../settings/settings.page';
 
 const routes: Routes = [
   {
